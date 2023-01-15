3ee09e2da7e750ef62b4acca1e39feb2c1ddcef2
ITMaaS <86196356+ITMaaS@users.noreply.github.com>
2023-01-15 14:55:03 +0000

    Add files via upload

---
+++ VLC/VLC-Remediation.ps1
@@ -0,0 +1,56 @@
+try{
+[CmdletBinding()]
+$ShortcutTargetPath = "C:\Program Files\VideoLAN\VLC\vlc.exe"
+
+$ShortcutDisplayName = "VLC media player"
+
+$PinToStart= "true"
+
+$IconFile= "C:\Program Files\VideoLAN\VLC\vlc.exe, 0"
+
+$ShortcutArguments= $null
+
+$WorkingDirectory= $null
+
+
+#helper function to avoid uneccessary code
+function Add-Shortcut {
+    param (
+        [Parameter(Mandatory)]
+        [String]$ShortcutTargetPath,
+        [Parameter(Mandatory)]
+        [String] $DestinationPath,
+        [Parameter()]
+        [String] $WorkingDirectory
+    )
+
+    process{
+        $WshShell = New-Object -comObject WScript.Shell
+        $Shortcut = $WshShell.CreateShortcut($destinationPath)
+        $Shortcut.TargetPath = $ShortcutTargetPath
+        $Shortcut.Arguments = $ShortcutArguments
+        $Shortcut.WorkingDirectory = $WorkingDirectory
+    
+        if ($IconFile){
+            $Shortcut.IconLocation = $IconFile
+        }
+        # Create the shortcut
+        $Shortcut.Save()
+        #cleanup
+        [Runtime.InteropServices.Marshal]::ReleaseComObject($WshShell) | Out-Null
+    }
+}
+
+
+            $startMenuDir= Join-Path $env:ALLUSERSPROFILE "Microsoft\Windows\Start Menu\Programs\VideoLAN\VLC"
+
+
+
+  $destinationPath = Join-Path -Path $startmenudir -ChildPath "$shortcutDisplayName.lnk"
+    Add-Shortcut -DestinationPath $destinationPath -ShortcutTargetPath $ShortcutTargetPath -WorkingDirectory $WorkingDirectory
+    Write-Output "Created shortcut to $ShortcutDisplayName"
+    Exit 0
+}
+Catch
+{Write-Output "Failed to create shortcut for $ShortcutDisplayName"
+Exit 1}
